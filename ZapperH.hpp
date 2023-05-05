8b2ea2942446727532de123255a11426b8a23ca0
Afsah Hyder <ah07065@st.habib.edu.pk>
2023-05-05 18:27:02 +0500

    Score booster implemented

--- ZapperH.hpp
+++ ZapperH.hpp
@@ -6,21 +6,14 @@
 
 
 class Zapper_h:public Killers, public Unit{
-
     SDL_Rect src, mover;
     int frame=0;
-  
     public:
     Zapper_h(SDL_Renderer* rend, SDL_Texture* ast, SDL_Rect mov);
     void draw();
-    
     int zapper_x = mover.x;
-    // int barry_y;
     bool collision(int barry_x, int barry_y) override;
-    
     void animation();
     ~Zapper_h();
     bool delete_item() override;
-
-
 };
--- ZapperV.hpp
+++ ZapperV.hpp
@@ -8,18 +8,12 @@ class Zapper_v:public Killers, public Unit{
 
     SDL_Rect src, mover;
     int frame=0;
-  
     public:
     Zapper_v(SDL_Renderer* rend, SDL_Texture* ast, SDL_Rect mov);
     void draw();
-    
     int zapper_x = mover.x;
-    // int barry_y;
     bool collision(int barry_x, int barry_y) override;
-    
     void animation();
     ~Zapper_v();
     bool delete_item() override;
-
-
 };
